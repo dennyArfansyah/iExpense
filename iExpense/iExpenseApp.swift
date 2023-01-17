5d7f33c6b8bdc147575e9731fd58c4c797abc3bd
dennyArfansyah <dennyarfansyah@gmail.com>
2023-01-17 17:48:36 +0700

    Initial Commit

---
+++ iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Denny Arfansyah on 17/01/23.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
